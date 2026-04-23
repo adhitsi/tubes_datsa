4b0f7ee200a1e69cfc7c60066b4916b047d2dfaa
adhitsi <f52124062@untad.ac.id>
2026-04-23 21:04:13 +0800

    update model ml

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ elif page == "Laporan":
 
         X = pd.get_dummies(X, drop_first=True)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         models = {
             "Linear Regression": LinearRegression(),
